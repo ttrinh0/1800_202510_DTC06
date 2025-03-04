28c766b1db5e0b898128f54bb64f43cf6ee38fe0
TIN TRINH <tin.ttrinh0@gmail.com>
2025-03-03 20:07:41 -0800

    Made navbar for non logged in users

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 function loadSkeleton() {
     console.log($('#navbarTop').load('./components/nav_top.html'));
+    console.log($('#navbarTopNonUser').load('./components/nav_top_non_user.html'));
     console.log($('#navbarBottom').load('./components/nav_bottom.html'));
     console.log($('#footer').load('./components/footer.html'));
 }
