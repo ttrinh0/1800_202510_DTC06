1b350dc53b433f68d99e5e00e45870722195c464
Joshua Sopena <jisopena25@gmail.cm>
2025-03-10 18:38:59 -0700

    Implement unfinished code  snippet from demo

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,24 @@ function logout() {
 }
 
 
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeHikeLoop(max) {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var storesRef = db.collection("stores");
+    for (i = 1; i <= max; i++) {
+        storesRef.add({ //add to database, autogen ID
+            name: "hike" + i,
+            details: "Elmo says this hike is amazing!  You will love going on hike" + i,
+            lat: 49 + i,    //randomly different
+            lng: -122 + i,  //randomly different
+            last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        });
+    }
+}
+
+
 //------------------------------------------------
 // Load carousel html components into other html
 //-------------------------------------------------
@@ -18,4 +36,6 @@ function Carousel() {
     console.log($('.storeCarousel').load('./components/store_carousel.html'));
     console.log($('.storeProductCarousel').load('./components/store_product_carousel.html'));
 }
+
+// writeHikeLoop(1)
 Carousel();
