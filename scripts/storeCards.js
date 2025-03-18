f320d3f459c5316f2aace136ac6a0ae2c09fc273
TIN TRINH <tin.ttrinh0@gmail.com>
2025-03-18 15:22:24 -0700

    Start store with product card

--- scripts/storeCards.js
+++ scripts/storeCards.js
@@ -19,3 +19,23 @@ function displayCardsDynamically(collection) {
 }
 
 displayCardsDynamically("stores");
+
+function displayProductCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("storeProductCardTemplate");
+    db.collection(collection).get()
+        .then(allStores => {
+            allStores.forEach(doc => {
+                var title = doc.data().name;
+                var details = doc.data().details;
+                var storeCode = doc.data().code;
+                var storeAddress = doc.data().address;
+                // var storeType = doc.data().storetype;
+                let newcard = cardTemplate.content.cloneNode(true);
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-address').innerHTML = storeAddress;
+                newcard.querySelector('.card-image').src = `./images/${storeCode}.jpg`;
+                document.getElementById("stores-products-go-here-go-here").appendChild(newcard);
+            })
+        })
+}
