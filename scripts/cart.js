644056580c1134251fc77dd820872fbbe5f60344
TIN TRINH <tin.ttrinh0@gmail.com>
2025-03-25 00:44:40 -0700

    Create function to put product card in cart page

--- scripts/cart.js
+++ scripts/cart.js
@@ -11,3 +11,33 @@ function addItemToCart() {
     // }
     localStorage.setItem('secondServeCart', JSON.stringify(productCart));
 }
+
+function showItemInCart() {
+    const cart = document.getElementById("cart")
+    productInfo = JSON.parse(localStorage.getItem("secondServeCart"))
+    title = Object.keys(productInfo)[0]
+    price = Object.values(productInfo)[0]
+    console.log(productInfo)
+    console.log(title)
+    console.log("price", price)
+    if (localStorage.length > 0) {
+        cart.innerHTML = ""
+        cart.classList.toggle("flex-grow")
+    }
+    for (var i = 0; i < localStorage.length; i++) {
+        let cartCard = `<div class="border-2 border-black/15 rounded-md bg-white drop-shadow-sm m-4">
+                    <div class="grid grid-cols-7 items-center text-center p-4">
+                        <img src="/images/c_bagel.jpg" alt=""
+                            class="border-2 border-black/10 drop-shadow-sm rounded-md object-cover object-center w-30 h-30">
+                        <p class="name-item text-left col-span-2 ml-4">${title}</p>
+                        <p>Quantity: <span>3</span></p>
+                        <p>Price: <span>3</span></p>
+                        <p>Total: <span>3</span></p>
+                        <i class="drop-shadow-sm fa-solid fa-xmark fa-lg text-right mr-5"></i>
+                    </div>
+                </div>`
+        cart.innerHTML += cartCard;
+        counter++
+    }
+}
+showItemInCart()
