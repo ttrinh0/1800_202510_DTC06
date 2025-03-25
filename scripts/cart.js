2f0137fda6831da0045c69da108b4c339f6fbf2e
TIN TRINH <tin.ttrinh0@gmail.com>
2025-03-25 00:05:55 -0700

    Add base cart function

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,13 @@
-function addItemtoCart() {
-    cart = document.getElementById("cart");
-    cart.innerHTML = "Hello";
-};
-
-// addItemtoCart();
+function addItemToCart() {
+    title = document.getElementsByClassName('.card-title');
+    console.log(title)
+    storename = document.getElementsByClassName('.store-title');
+    p_after = document.getElementsByClassName('.p_after');
+    productCart = { "Bagel": { "quantity": 1, "store": "Tim Hortons", "price": "1.00" } }
+    // store = cards.querySelector('.store-title')
+    // p_after = cards.querySelector('.store-title')
+    // return {
+    //     title: title.textContext
+    // }
+    localStorage.setItem('secondServeCart', JSON.stringify(productCart));
+}
