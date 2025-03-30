f23272d3859d4caaa505c03d0269fb29d8355ed1
TIN TRINH <tin.ttrinh0@gmail.com>
2025-03-30 01:02:43 -0700

    Add functionality to remove items from cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,17 +1,17 @@
 
 
 function showItemInCart() {
-    const cart = document.getElementById("cart")
+    let cart = document.getElementById("cart")
+    let cartEmpty = document.getElementById("cartEmpty")
 
     if ('secondServeCart' in localStorage) {
-        cart.innerHTML = ""
+        cartEmpty.innerHTML = ""
         cart.classList.toggle("flex-grow")
     }
 
     let subtotal = 0
 
     productInfo = JSON.parse(localStorage.getItem('secondServeCart'))
-    console.log("product: ", productInfo)
     cartLength = Object.keys(productInfo).length
 
     for (i = 0; i < cartLength; i++) {
@@ -20,22 +20,69 @@ function showItemInCart() {
         img = Object.values(productInfo)[i]["img"]
         quantity = Object.values(productInfo)[i]["quantity"]
         subtotal += Number(price)
-        let cartCard = `<div class="border-2 border-black/15 rounded-md bg-white drop-shadow-sm mt-4">
-                        <div class="grid grid-cols-6 items-center text-center p-4">
-                            <img src="/images/${img}.jpg" alt=""
-                                class="border-2 border-black/10 drop-shadow-sm rounded-md object-cover object-center w-30 h-30">
-                            <p class="name-item text-left col-span-2 ml-4">${title}</p>
-                            <p>Quantity: <span>${quantity}</span></p>
-                            <p>Price: $${price}</p>
-                            <i class="drop-shadow-sm fa-solid fa-xmark fa-lg mr-5"></i>
-                        </div>
-                    </div>`
+        cardNumberID = "cartCard" + i
+        cardNumberRemoveID = "cartCardRemove" + i
+
+        let cartCard = `<div id="${cardNumberID}" class="border-2 border-black/15 rounded-md bg-white drop-shadow-sm mt-4">
+            <div class="p-4 flex justify-between">
+                <img src="/images/${img}.jpg" alt=""
+                    class="border-2 border-black/10 drop-shadow-sm rounded-md object-cover object-center w-30 h-30">
+                <div class="my-auto flex-grow ml-4">
+                    <p class="name-item text-left col-span-2 font-semibold">${title}</p>
+                    <p class="text-sm">Quantity: <span>${quantity}</span></p>
+                    <p class="text-sm">Price: $${price}</p>
+                </div>
+                <button id="${cardNumberRemoveID}">
+                    <i class="mr-2 drop-shadow-sm fa-solid fa-xmark fa-lg my-auto cursor-pointer p-2"></i>
+                </button>
+            </div>
+        </div>`
         cart.innerHTML += cartCard;
     }
+
     let subtotaldiv = document.getElementById("subtotal")
     subtotaldiv.innerHTML = "Subtotal: $" + subtotal.toFixed(2)
+    let checkoutdiv = document.getElementById("checkout")
+    checkoutdiv.innerHTML = `<button
+    class="cursor-pointer w-[120px] bg-[#439189] text-white font-semibold py-2 px-4 rounded-lg transition hover:bg-[#f6d276] hover:text-[#276861]"
+    onclick="storeItemsToOrderHistory()">Checkout</button>`
+
+    for (i = 0; i < cartLength; i++) {
+
+        let cartCard = document.getElementById("cartCard" + i)
+        let cartEmpty = document.getElementById("cartEmpty")
+        newCounter = localStorage.getItem('counter');
+        let cardNumberRemove = document.getElementById("cartCardRemove" + i)
+        let numberi = i
+        let title = Object.keys(productInfo)[numberi]
+        cardNumberRemove.addEventListener("click", () => {
+            const updatedObj = Object.fromEntries(
+                Object.entries(productInfo).filter(([key]) => key !== title)
+            );
+            productInfo = updatedObj
+            cartCard.remove()
+            newCounter--
+            if (newCounter == 0) {
+                localStorage.removeItem('secondServeCart');
+                subtotaldiv.innerHTML = ""
+                checkoutdiv.innerHTML = ""
+                cartEmpty.innerHTML = `<i class="fa-solid fa-cart-shopping text-3xl mx-auto"></i>
+                    <h4 class="font-semibold text-lg pt-5 mx-auto">Your cart is empty</h4>
+                    <p class="text-center mx-auto">Once you add items from a restaurant or store, your cart will appear
+                        here.
+                    </p>`
+                cart.classList.toggle("flex-grow")
+            } else {
+                localStorage.setItem('secondServeCart', JSON.stringify(updatedObj));
+            }
+            localStorage.setItem('counter', newCounter);
+        })
+    }
 }
 
+function removeItemCart() {
+    console.log("h")
+}
 
 function getNameFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
